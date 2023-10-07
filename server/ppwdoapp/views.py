e03e5ac6b1055f3cc0aef33f9a3b488c78418754
Iacopo Modica <iacopo.modica@gmail.com>
2023-10-07 11:17:01 +0200

    introduced html render + migrations

--- server/ppwdoapp/views.py
+++ server/ppwdoapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world")
+    return render(request, 'home.html', {"test":{'0': 'a', '1':'b'}})
