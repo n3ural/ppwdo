ad0620d36f86df7825b14f03f1f423d1c5e11420
Iacopo Modica <iacopo.modica@gmail.com>
2023-10-08 10:14:48 +0200

    redirect nonexistent routes to specified page

--- server/ppwdo/urls.py
+++ server/ppwdo/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('ppwdoapp', include('ppwdoapp.urls'))
-    path('', include('ppwdoapp.urls'))
-
+    path('', include('ppwdoapp.urls')),
+    path('<path:unmatched_path>/', RedirectView.as_view(url='/', permanent=False)),
 ]
